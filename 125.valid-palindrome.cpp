acca166413bed42913aee1fdd82eb8ec253bf8ed
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2018-12-15 01:29:46 +0530

    update 125.valid-palindrome.cpp

---
+++ 125.valid-palindrome.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=125 lang=cpp
+ *
+ * [125] Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (29.29%)
+ * Total Accepted:    293.5K
+ * Total Submissions: 1M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases.
+ *
+ * Note: For the purpose of this problem, we define empty string as valid
+ * palindrome.
+ *
+ * Example 1:
+ *
+ *
+ * Input: "A man, a plan, a canal: Panama"
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: "race a car"
+ * Output: false
+ *
+ *
+ */
+class Solution {
+public:
+    bool isalphanum(char c){
+      return ('0' <= c && c<= '9') || ('a' <=c && c <= 'z') || ('A' <=c && c <= 'Z');
+    }
+    char lower(char c){
+      if('A' <=c && c <= 'Z')
+        return c - 'A' + 'a';
+      return c;
+    }
+    bool isPalindrome(string s) {
+        int i=0;
+        int j = s.size()-1;
+        if(j == -1) return true;
+        while(i<j){
+          if(!isalphanum(s[i]))  {  i++; continue; }
+          if(!isalphanum(s[j]))  {  j--; continue; }
+          if(lower(s[i]) != lower(s[j])){
+            return false;
+          }
+          i++; j--;
+        }
+        return true;
+    }
+};
