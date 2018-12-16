5008641beac3b8e628d87af9dc47770aaf868bf8
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2018-12-16 13:11:23 +0530

    update 88.merge-sorted-array.cpp

--- 88.merge-sorted-array.cpp
+++ 88.merge-sorted-array.cpp
@@ -37,28 +37,23 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0, j=0, k=0;
-        vector<int> temp;
-        while(j<n && i<m){
-            if(nums1[i] <= nums2[j]){
-                temp.push_back(nums1[i]);
-                i++;
+        int i=m-1, j=n-1;
+        int k = m+n-1;
+        while(j>=0 && i>=0){
+            cout<<i<<":"<<j<<":"<<k<<" ";
+            if(nums2[j] > nums1[i]){
+                nums1[k] = nums2[j];
+                j--;k--;
             }else{
-                temp.push_back(nums2[j]);
-                j++;
+                nums1[k] = nums1[i];
+                i--;k--;
             }
         }
-        while(i<m){
-            temp.push_back(nums1[i]);
-            i++;
-        }
-        while(j<n){
-            temp.push_back(nums2[j]);
-            j++;
-        }
-        // for(auto e: temp)   cout<<e<<" ";
-        for(i=0;i<m+n;i++){
-            nums1[i] = temp[i];
+        if(i==-1){
+            while(k>=0){
+                nums1[k] = nums2[j];
+                j--;k--;
+            }
         }
     }
 };
