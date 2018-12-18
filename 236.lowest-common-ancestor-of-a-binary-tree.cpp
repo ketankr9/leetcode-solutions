92aa779c702fb6dc6a35a3bae51afeda0d4b080d
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2018-12-18 15:48:30 +0530

    update 236.lowest-common-ancestor-of-a-binary-tree.cpp

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=236 lang=cpp
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (33.65%)
+ * Total Accepted:    222.5K
+ * Total Submissions: 658.5K
+ * Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+ *
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given
+ * nodes in the tree.
+ * 
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of nodes 5 and 1 is 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * Output: 5
+ * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant
+ * of itself according to the LCA definition.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the binary tree.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* ans;
+    bool found;
+    bool callme(TreeNode*root, TreeNode* p, TreeNode* q){
+        if(!root || found)   return false;
+
+        bool isroot = false;
+        if(root == p || root == q)
+            isroot = true;
+
+        bool leftfound = callme(root->left, p, q);
+        bool rightfound = callme(root->right, p, q);
+        
+        if((leftfound && rightfound) || ((leftfound^rightfound)&& isroot)){
+            ans = root;
+            found = true;
+        }
+
+        return leftfound || rightfound || isroot;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        found = false;
+        callme(root, p, q);
+        return ans;
+    }
+};
