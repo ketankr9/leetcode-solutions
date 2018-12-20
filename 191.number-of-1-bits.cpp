2634b0de968de8db32f2fa83d12bda5c708e0171
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2018-12-20 09:15:02 +0530

    update 191.number-of-1-bits.cpp

--- 191.number-of-1-bits.cpp
+++ 191.number-of-1-bits.cpp
@@ -67,9 +67,9 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count = 0;
-        while(n > 0){
-            count += n & 1;
-            n = n>>1;
+        while(n != 0){
+            count += 1; // n & 1;
+            n = n & n-1;
         }
         return count;
     }
