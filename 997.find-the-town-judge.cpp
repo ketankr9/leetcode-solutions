da2aa80ce37a6481d40bbfa911ddda9793e7304a
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-03-10 17:02:35 +0530

    update 997.find-the-town-judge.cpp

---
+++ 997.find-the-town-judge.cpp
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode id=997 lang=cpp
+ *
+ * [997] Find the Town Judge
+ *
+ * https://leetcode.com/problems/find-the-town-judge/description/
+ *
+ * algorithms
+ * Easy (47.73%)
+ * Total Accepted:    7.5K
+ * Total Submissions: 15.6K
+ * Testcase Example:  '2\n[[1,2]]'
+ *
+ * In a town, there are N people labelled from 1 to N.  There is a rumor that
+ * one of these people is secretly the town judge.
+ * 
+ * If the town judge exists, then:
+ * 
+ * 
+ * The town judge trusts nobody.
+ * Everybody (except for the town judge) trusts the town judge.
+ * There is exactly one person that satisfies properties 1 and 2.
+ * 
+ * 
+ * You are given trust, an array of pairs trust[i] = [a, b] representing that
+ * the person labelled a trusts the person labelled b.
+ * 
+ * If the town judge exists and can be identified, return the label of the town
+ * judge.  Otherwise, return -1.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: N = 2, trust = [[1,2]]
+ * Output: 2
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: N = 3, trust = [[1,3],[2,3]]
+ * Output: 3
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+ * Output: -1
+ * 
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: N = 3, trust = [[1,2],[2,3]]
+ * Output: -1
+ * 
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+ * Output: 3
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= N <= 1000
+ * trust.length <= 10000
+ * trust[i] are all different
+ * trust[i][0] != trust[i][1]
+ * 1 <= trust[i][0], trust[i][1] <= N
+ * 
+ * 
+ */
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        if(N == 0)
+            return -1;
+        int tCount[N];
+        bool judge[N];
+        fill(tCount, tCount+N, 0);
+        fill(judge, judge+N, true);
+
+        for(int i=0;i<(int)trust.size();i++){
+            tCount[trust[i][1]-1]++;
+            judge[trust[i][0]-1] = false;
+        }
+        for(int i=0; i<N; i++){
+            if(tCount[i] == N-1 && judge[i]){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
