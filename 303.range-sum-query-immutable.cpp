737be56c2c1cfdd46d99b982a17af97e5765d53f
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-06-05 17:42:57 +0530

    update 303.range-sum-query-immutable.cpp

---
+++ 303.range-sum-query-immutable.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=303 lang=cpp
+ *
+ * [303] Range Sum Query - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (36.62%)
+ * Total Accepted:    139K
+ * Total Submissions: 365.4K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * Given an integer array nums, find the sum of the elements between indices i
+ * and j (i ≤ j), inclusive.
+ * 
+ * Example:
+ * 
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
+ * 
+ * 
+ */
+class NumArray {
+public:
+    vector<int> rang;
+    NumArray(vector<int>& nums) {
+        rang.clear();
+        rang.push_back(0);
+        for(int i=0; i<(int)nums.size(); i++){
+            rang.push_back(nums[i] + rang[i]);
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return rang[j+1]-rang[i];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
