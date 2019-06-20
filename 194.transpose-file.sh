ea3e13580c88dc21e3b95111e9ebb46f4a0cf896
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-06-20 23:05:54 +0530

    update 194.transpose-file.sh

--- 194.transpose-file.sh
+++ 194.transpose-file.sh
@@ -1,6 +1,7 @@
 # Read from the file file.txt and print its transposed content to stdout.
 num=1
-while [ ! -z "$(cat file.txt | awk -v n="$num" '{print $n}' | tr '\r\n' ' ' | xargs echo -n )" ]; do
+ncol="$(head -n1 file.txt | wc -w)"
+while [ "$num" -le "$ncol" ]; do
 echo "$(cat file.txt | awk -v n="$num" '{print $n}' | tr '\r\n' ' ' | xargs echo -n )";
 num=$(($num+1))
 done
