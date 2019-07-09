d1086ff7144d8fab98fff500b447088a7ee40b0b
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-07-09 23:55:58 +0530

    update 60.permutation-sequence.cpp

---
+++ 60.permutation-sequence.cpp
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=60 lang=cpp
+ *
+ * [60] Permutation Sequence
+ *
+ * https://leetcode.com/problems/permutation-sequence/description/
+ *
+ * algorithms
+ * Medium (33.44%)
+ * Likes:    880
+ * Dislikes: 230
+ * Total Accepted:    142K
+ * Total Submissions: 424.7K
+ * Testcase Example:  '3\n3'
+ *
+ * The set [1,2,3,...,n] contains a total of n! unique permutations.
+ * 
+ * By listing and labeling all of the permutations in order, we get the
+ * following sequence for n = 3:
+ * 
+ * 
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 
+ * 
+ * Given n and k, return the k^th permutation sequence.
+ * 
+ * Note:
+ * 
+ * 
+ * Given n will be between 1 and 9 inclusive.
+ * Given k will be between 1 and n! inclusive.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 3, k = 3
+ * Output: "213"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 4, k = 9
+ * Output: "2314"
+ * 
+ * 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long fact(long long a){
+        if(a<=1)    return 1;
+        return a*fact(a-1);
+    }
+    int kth(int x, vector<bool> &used){
+        int i = 0, c=0;
+        while(c<=x && i<(int)used.size()){
+            if(!used[i]){
+                if(c == x){
+                    used[i] = true;    
+                    return i;
+                }
+                c++;
+            }
+            i++;
+        }
+    }
+    string getPermutation(int n, int k) {
+        vector<int> used;
+        for(int p=1;p<=n;p++)
+            used.push_back(p);
+        string ans = "";
+        int i=n-1;
+        while(k>0){
+            long long x = k/fact(i);
+            // cout<<k<<x<<"; ";
+            k -= x*fact(i--);
+            if(k == 0){
+                ans = ans + to_string(used[x-1]);
+                used.erase(used.begin()+x-1);
+                reverse(used.begin(), used.end());
+            }else{
+                ans = ans + to_string(used[x]);//to_string(kth(x, used));
+                used.erase(used.begin()+x);
+            }
+        }
+        for(auto e: used)
+            ans = ans + to_string(e);
+        return ans;
+    }
+};
+
