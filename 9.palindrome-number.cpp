b427d478a4ea2979a67ee6b1a6d31aa14164e354
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-07-09 17:04:01 +0530

    update 9.palindrome-number.cpp

---
+++ 9.palindrome-number.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (43.87%)
+ * Likes:    1496
+ * Dislikes: 1325
+ * Total Accepted:    613.2K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '121'
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome
+ * when it reads the same backward as forward.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 121
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ * 
+ * 
+ * Follow up:
+ * 
+ * Coud you solve it without converting the integer to a string?
+ * 
+ */
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int t = x;
+        long c = 0;
+        while(t>0){
+            c = c*10 + t%10;
+            t/=10;
+        }
+        return c<=(long)INT_MAX && c == x;
+    }
+};
+
