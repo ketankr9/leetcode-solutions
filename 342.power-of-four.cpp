c2d04eba037f7aa796e8ab649179744685baf893
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-07-17 23:59:23 +0530

    update 342.power-of-four.cpp

---
+++ 342.power-of-four.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=342 lang=cpp
+ *
+ * [342] Power of Four
+ *
+ * https://leetcode.com/problems/power-of-four/description/
+ *
+ * algorithms
+ * Easy (40.49%)
+ * Total Accepted:    117.8K
+ * Total Submissions: 290.9K
+ * Testcase Example:  '16'
+ *
+ * Given an integer (signed 32 bits), write a function to check whether it is a
+ * power of 4.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 16
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 5
+ * Output: false
+ * 
+ * 
+ * Follow up: Could you solve it without loops/recursion?
+ */
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        long long n = 1;
+        while(n<(long long)num)
+            n = n<<2;
+        return n == (long long)num;
+    }
+};
