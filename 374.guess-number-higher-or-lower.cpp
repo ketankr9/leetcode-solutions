00310f8b6319c66b160b4e8b9c59f73efd45a98d
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-07-28 23:43:41 +0530

    update 374.guess-number-higher-or-lower.cpp

---
+++ 374.guess-number-higher-or-lower.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=374 lang=cpp
+ *
+ * [374] Guess Number Higher or Lower
+ *
+ * https://leetcode.com/problems/guess-number-higher-or-lower/description/
+ *
+ * algorithms
+ * Easy (39.91%)
+ * Total Accepted:    113.3K
+ * Total Submissions: 283.8K
+ * Testcase Example:  '10\n6'
+ *
+ * We are playing the Guess Game. The game is as follows:
+ * 
+ * I pick a number from 1 to n. You have to guess which number I picked.
+ * 
+ * Every time you guess wrong, I'll tell you whether the number is higher or
+ * lower.
+ * 
+ * You call a pre-defined API guess(int num) which returns 3 possible results
+ * (-1, 1, or 0):
+ * 
+ * 
+ * -1 : My number is lower
+ * ⁠1 : My number is higher
+ * ⁠0 : Congrats! You got it!
+ * 
+ * 
+ * Example :
+ * 
+ * 
+ * 
+ * Input: n = 10, pick = 6
+ * Output: 6
+ * 
+ * 
+ * 
+ */
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1, high = n;
+        long mid = ((long)low+high)/2;
+        int flag = guess((int)mid);
+        while(flag != 0){
+            if(flag == -1){
+                high = (int)mid-1;
+            }else{ // flag == 1
+                low = (int)mid+1;
+            }
+            mid = ((long)low+high)/2;
+            flag = guess((int)mid);
+        }
+
+        return mid;
+    }
+};
