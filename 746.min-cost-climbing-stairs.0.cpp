061bec74a5223d85539a4ad7bf86350cad9dc627
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-07-30 11:12:35 +0530

    update 746.min-cost-climbing-stairs.0.cpp

---
+++ 746.min-cost-climbing-stairs.0.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=746 lang=cpp
+ *
+ * [746] Min Cost Climbing Stairs
+ *
+ * https://leetcode.com/problems/min-cost-climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (47.76%)
+ * Total Accepted:    89.8K
+ * Total Submissions: 188.1K
+ * Testcase Example:  '[0,0,0,0]'
+ *
+ * 
+ * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0
+ * indexed).
+ * 
+ * Once you pay the cost, you can either climb one or two steps. You need to
+ * find minimum cost to reach the top of the floor, and you can either start
+ * from the step with index 0, or the step with index 1.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: cost = [10, 15, 20]
+ * Output: 15
+ * Explanation: Cheapest is start on cost[1], pay that cost and go to the
+ * top.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * Output: 6
+ * Explanation: Cheapest is start on cost[0], and only step on 1s, skipping
+ * cost[3].
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * cost will have a length in the range [2, 1000].
+ * Every cost[i] will be an integer in the range [0, 999].
+ * 
+ * 
+ */
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int first = cost[0], second = cost[1];
+        for(int i=2; i<(int)cost.size(); i++){
+            int next = cost[i] + min(first, second);
+            first = second;
+            second = next;
+        }
+        return min(second, first);
+    }
+};
