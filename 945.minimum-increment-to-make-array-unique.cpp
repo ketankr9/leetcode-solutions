fb913af91b7c775b6ac39bedad1cb49bc0f5cd59
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-08-06 14:31:11 +0530

    update 945.minimum-increment-to-make-array-unique.cpp

---
+++ 945.minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=945 lang=cpp
+ *
+ * [945] Minimum Increment to Make Array Unique
+ *
+ * https://leetcode.com/problems/minimum-increment-to-make-array-unique/description/
+ *
+ * algorithms
+ * Medium (43.31%)
+ * Total Accepted:    12.2K
+ * Total Submissions: 28.3K
+ * Testcase Example:  '[1,2,2]'
+ *
+ * Given an array of integers A, a move consists of choosing any A[i], and
+ * incrementing it by 1.
+ * 
+ * Return the least number of moves to make every value in A unique.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,2]
+ * Output: 1
+ * Explanation:  After 1 move, the array could be [1, 2, 3].
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,2,1,2,1,7]
+ * Output: 6
+ * Explanation:  After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+ * It can be shown with 5 or less moves that it is impossible for the array to
+ * have all unique values.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 0 <= A.length <= 40000
+ * 0 <= A[i] < 40000
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    int find(unordered_map<int, int>& mm, int x){
+        // cout<<x<<" ";
+        if(mm.find(x)!=mm.end()){
+            mm[x] = find(mm, mm[x]);
+            return mm[x];
+        }
+        return x;
+    }
+    int minIncrementForUnique(vector<int>& A) {
+        unordered_map<int, int> next;
+        int ans = 0;
+        for(auto e: A){
+            if(next.find(e)==next.end()){
+                next[e] = e+1;
+            }else{
+                int x = e;
+                next[x] = find(next, x);
+                // cout<<x<<">"<<next[x]<<" ";
+                next[next[x]] = next[x]+1;
+                
+                ans += next[x] - x;
+            }
+        }
+        return ans;
+    }
+};
