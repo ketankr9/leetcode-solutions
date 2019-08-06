6c7c09131c3ebbf143d467a2631247ae239e9318
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-08-06 23:36:24 +0530

    update 983.minimum-cost-for-tickets.cpp

--- 983.minimum-cost-for-tickets.cpp
+++ 983.minimum-cost-for-tickets.cpp
@@ -81,14 +81,13 @@ public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         unordered_set<int> ss;
         for(auto e: days)   ss.insert(e);
-        int dp[367];
-        dp[0] = 0;
+        int dp[31];
         for(int i=days.front(); i<=days.back(); i++){
             if(ss.find(i)==ss.end())
-                dp[i] = (i-1<days.front()?0:dp[i-1]);
+                dp[i%30] = (i-1<days.front()?0:dp[(i-1+30)%30]);
             else
-                dp[i] = min( (i-1<days.front()?0:dp[i-1])+costs[0] , min( (i-7<days.front()?0:dp[i-7])+costs[1], (i-30<days.front()?0:dp[i-30])+costs[2]));
+                dp[i%30] = min( (i-1<days.front()?0:dp[(i-1+30)%30])+costs[0] , min( (i-7<days.front()?0:dp[(i-7+30)%30])+costs[1], (i-30<days.front()?0:dp[(i-30+30)%30])+costs[2]));
         }
-        return dp[days.back()];
+        return dp[days.back()%30];
     }
 };
