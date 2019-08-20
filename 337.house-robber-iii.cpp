61d99cfe0cf531f976a93e8655e1797ef769b93a
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-08-20 14:55:27 +0530

    update 337.house-robber-iii.cpp

---
+++ 337.house-robber-iii.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=337 lang=cpp
+ *
+ * [337] House Robber III
+ *
+ * https://leetcode.com/problems/house-robber-iii/description/
+ *
+ * algorithms
+ * Medium (48.50%)
+ * Total Accepted:    109.8K
+ * Total Submissions: 226.2K
+ * Testcase Example:  '[3,2,3,null,3,null,1]'
+ *
+ * The thief has found himself a new place for his thievery again. There is
+ * only one entrance to this area, called the "root." Besides the root, each
+ * house has one and only one parent house. After a tour, the smart thief
+ * realized that "all houses in this place forms a binary tree". It will
+ * automatically contact the police if two directly-linked houses were broken
+ * into on the same night.
+ * 
+ * Determine the maximum amount of money the thief can rob tonight without
+ * alerting the police.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,2,3,null,3,null,1]
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \   \ 
+ * ⁠    3   1
+ * 
+ * Output: 7 
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,5,1,3,null,1]
+ * 
+ * 3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \   \ 
+ * ⁠1   3   1
+ * 
+ * Output: 9
+ * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	unordered_map<TreeNode*, int> mm;
+	int callme(TreeNode* root){
+		if(root == NULL)	return 0;
+		if(mm.find(root)!=mm.end())
+			return mm[root];
+		return mm[root] = max(
+			root->val + 
+			(root->left!=NULL?(callme(root->left->left) + callme(root->left->right)):0) + 
+			(root->right!=NULL?(callme(root->right->left) + callme(root->right->right)):0),
+			callme(root->left) + callme(root->right)
+			);
+	}
+
+    int rob(TreeNode* root) {
+        mm.clear();
+        return callme(root);
+    }
+};
