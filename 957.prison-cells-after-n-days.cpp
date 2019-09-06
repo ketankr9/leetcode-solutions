59e0d8ed47bb0c38c26f5a6094d89e53a7c1df11
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-09-07 00:50:00 +0530

    update 957.prison-cells-after-n-days.cpp

---
+++ 957.prison-cells-after-n-days.cpp
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode id=957 lang=cpp
+ *
+ * [957] Prison Cells After N Days
+ *
+ * https://leetcode.com/problems/prison-cells-after-n-days/description/
+ *
+ * algorithms
+ * Medium (37.42%)
+ * Total Accepted:    20.2K
+ * Total Submissions: 53.7K
+ * Testcase Example:  '[0,1,0,1,1,0,0,1]\n7'
+ *
+ * There are 8 prison cells in a row, and each cell is either occupied or
+ * vacant.
+ * 
+ * Each day, whether the cell is occupied or vacant changes according to the
+ * following rules:
+ * 
+ * 
+ * If a cell has two adjacent neighbors that are both occupied or both vacant,
+ * then the cell becomes occupied.
+ * Otherwise, it becomes vacant.
+ * 
+ * 
+ * (Note that because the prison is a row, the first and the last cells in the
+ * row can't have two adjacent neighbors.)
+ * 
+ * We describe the current state of the prison in the following way: cells[i]
+ * == 1 if the i-th cell is occupied, else cells[i] == 0.
+ * 
+ * Given the initial state of the prison, return the state of the prison after
+ * N days (and N such changes described above.)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: cells = [0,1,0,1,1,0,0,1], N = 7
+ * Output: [0,0,1,1,0,0,0,0]
+ * Explanation: 
+ * The following table summarizes the state of the prison on each day:
+ * Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+ * Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+ * Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+ * Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+ * Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+ * Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+ * Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+ * Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+ * Output: [0,0,1,1,1,1,1,0]
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * cells.length == 8
+ * cells[i] is in {0, 1}
+ * 1 <= N <= 10^9
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& x, int N) {
+        
+        // convert 1st state as it is different from other states 
+        // by ability to have 1 at first and last position
+        vector<int> nextt(8, 0);
+        for(int j=1; j<7; j++)
+            nextt[j] = !(x[j-1]^x[j+1]);
+        x = nextt;
+        N--;
+
+        // find after how many days the states repeat
+        int i;
+        for(i=1; i<=(1<<8); i++){
+            vector<int> next(8, 0);
+            for(int j=1; j<7; j++)
+                next[j] = !(x[j-1]^x[j+1]);
+            x = next;
+            if(next == nextt)
+                break;
+        }
+
+        N %= i; // after %, N <= 256
+        for(i=0; i<N; i++){
+            vector<int> next(8, 0);
+            for(int j=1; j<7; j++)
+                next[j] = !(x[j-1]^x[j+1]);
+            x = next;
+        }
+        return x;
+
+    }
+};
