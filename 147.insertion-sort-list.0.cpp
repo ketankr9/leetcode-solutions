9978489f43fa224201755dee5f64c47dbfb46201
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-09-07 18:55:31 +0530

    update 147.insertion-sort-list.0.cpp

---
+++ 147.insertion-sort-list.0.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=147 lang=cpp
+ *
+ * [147] Insertion Sort List
+ *
+ * https://leetcode.com/problems/insertion-sort-list/description/
+ *
+ * algorithms
+ * Medium (35.61%)
+ * Total Accepted:    135.3K
+ * Total Submissions: 379.8K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * Sort a linked list using insertion sort.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * A graphical example of insertion sort. The partial sorted list (black)
+ * initially contains only the first element in the list.
+ * With each iteration one element (red) is removed from the input data and
+ * inserted in-place into the sorted list
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Algorithm of Insertion Sort:
+ * 
+ * 
+ * Insertion sort iterates, consuming one input element each repetition, and
+ * growing a sorted output list.
+ * At each iteration, insertion sort removes one element from the input data,
+ * finds the location it belongs within the sorted list, and inserts it
+ * there.
+ * It repeats until no input elements remain.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 4->2->1->3
+ * Output: 1->2->3->4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -1->5->3->4->0
+ * Output: -1->0->3->4->5
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+    	if(head == NULL)	return NULL;
+    	vector<ListNode*> vec;
+    	while(head != NULL){
+    		vec.push_back(head);
+    		head = head->next;
+    	}
+    	sort(vec.begin(), vec.end(), [](ListNode* x, ListNode* y){return x->val < y->val; });
+    	for(int i=0; i+1<vec.size(); i++)
+    		vec[i]->next = vec[i+1];
+    	vec[(int)vec.size()-1]->next = NULL;
+    	return vec[0];
+    }
+};
