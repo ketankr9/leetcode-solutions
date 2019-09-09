e5c47dfbd4dbfd0365c7b6deb805631f9e1bc735
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-09-09 21:51:36 +0530

    update 382.linked-list-random-node.cpp

---
+++ 382.linked-list-random-node.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=382 lang=cpp
+ *
+ * [382] Linked List Random Node
+ *
+ * https://leetcode.com/problems/linked-list-random-node/description/
+ *
+ * algorithms
+ * Medium (49.65%)
+ * Total Accepted:    55.3K
+ * Total Submissions: 111.4K
+ * Testcase Example:  '["Solution","getRandom"]\n[[[1,2,3]],[]]'
+ *
+ * Given a singly linked list, return a random node's value from the linked
+ * list. Each node must have the same probability of being chosen.
+ * 
+ * Follow up:
+ * What if the linked list is extremely large and its length is unknown to you?
+ * Could you solve this efficiently without using extra space?
+ * 
+ * 
+ * Example:
+ * 
+ * // Init a singly linked list [1,2,3].
+ * ListNode head = new ListNode(1);
+ * head.next = new ListNode(2);
+ * head.next.next = new ListNode(3);
+ * Solution solution = new Solution(head);
+ * 
+ * // getRandom() should return either 1, 2, or 3 randomly. Each element should
+ * have equal probability of returning.
+ * solution.getRandom();
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    ListNode* head;
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int ans;
+        ListNode* cur = head;
+        int k = 1;
+        while(cur){
+        	if(rand()%k++ == 0)
+        		ans = cur->val;
+        	cur = cur->next;
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
