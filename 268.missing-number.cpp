d9d91cd7c620726a0b89d26fa05a2fa0e4acba44
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-09-13 16:29:14 +0530

    update 268.missing-number.cpp

---
+++ 268.missing-number.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=268 lang=cpp
+ *
+ * [268] Missing Number
+ *
+ * https://leetcode.com/problems/missing-number/description/
+ *
+ * algorithms
+ * Easy (49.21%)
+ * Total Accepted:    316K
+ * Total Submissions: 642.1K
+ * Testcase Example:  '[3,0,1]'
+ *
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
+ * find the one that is missing from the array.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,0,1]
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * 
+ * 
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement
+ * it using only constant extra space complexity?
+ */
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+		int n = nums.size();
+		nums.push_back(n);
+		int missing;
+		for(int i=0; i<n; i++){
+			if(nums[i] == 0)
+				missing = i;
+			nums[abs(nums[i])] = -1*abs(nums[abs(nums[i])]);
+		}
+		for(int i=0; i<=n; i++)
+			if(nums[i] > 0)
+				return i;
+		return missing;
+    }
+};
