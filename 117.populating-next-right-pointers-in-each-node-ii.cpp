226d4ca5f7240d9c1cec072ed4acae91e9c62750
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-09-13 18:37:48 +0530

    update 117.populating-next-right-pointers-in-each-node-ii.cpp

---
+++ 117.populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=117 lang=cpp
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ *
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ *
+ * algorithms
+ * Medium (35.49%)
+ * Total Accepted:    199.4K
+ * Total Submissions: 562K
+ * Testcase Example:  '{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}'
+ *
+ * Given a binary tree
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ * 
+ * Output:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+ * 
+ * Explanation: Given the above binary tree (Figure A), your function should
+ * populate each next pointer to point to its next right node, just like in
+ * Figure B.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You may only use constant extra space.
+ * Recursive approach is fine, implicit stack space does not count as extra
+ * space for this problem.
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root == NULL)    return NULL;
+        Node* cur = root;
+        while(cur != NULL){
+            Node* head = NULL, *prev = NULL;
+            while(cur != NULL){
+                if(cur->left != NULL){
+                    if(head == NULL)
+                        head = cur->left;
+                    if(prev!=NULL)
+                        prev->next = cur->left;
+                    prev = cur->left;
+                }
+                if(cur->right != NULL){
+                    if(head == NULL)
+                        head = cur->right;
+                    if(prev!=NULL)
+                        prev->next = cur->right;
+                    prev = cur->right;
+                }
+                cur = cur->next;
+            }
+            cur = head;
+        }
+        return root;
+    }
+};
