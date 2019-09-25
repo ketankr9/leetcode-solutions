69af1db9ce2e663d829054fb9c9a23d0b4935817
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2019-09-25 19:56:49 +0530

    update 701.insert-into-a-binary-search-tree.cpp

---
+++ 701.insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=701 lang=cpp
+ *
+ * [701] Insert into a Binary Search Tree
+ *
+ * https://leetcode.com/problems/insert-into-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (77.10%)
+ * Total Accepted:    60.9K
+ * Total Submissions: 79K
+ * Testcase Example:  '[4,2,7,1,3]\n5'
+ *
+ * Given the root node of a binary search tree (BST) and a value to be inserted
+ * into the tree, insert the value into the BST. Return the root node of the
+ * BST after the insertion. It is guaranteed that the new value does not exist
+ * in the original BST.
+ * 
+ * Note that there may exist multiple valid ways for the insertion, as long as
+ * the tree remains a BST after insertion. You can return any of them.
+ * 
+ * For example, 
+ * 
+ * 
+ * Given the tree:
+ * ⁠       4
+ * ⁠      / \
+ * ⁠     2   7
+ * ⁠    / \
+ * ⁠   1   3
+ * And the value to insert: 5
+ * 
+ * 
+ * You can return this binary search tree:
+ * 
+ * 
+ * ⁠        4
+ * ⁠      /   \
+ * ⁠     2     7
+ * ⁠    / \   /
+ * ⁠   1   3 5
+ * 
+ * 
+ * This tree is also valid:
+ * 
+ * 
+ * ⁠        5
+ * ⁠      /   \
+ * ⁠     2     7
+ * ⁠    / \   
+ * ⁠   1   3
+ * ⁠        \
+ * ⁠         4
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* insert(TreeNode* root, int val){
+		if(root == NULL)	return new TreeNode(val);
+
+		if(root->val < val)
+			root->right = insert(root->right, val);
+		else
+			root->left = insert(root->left, val);
+		return root;
+	}
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        return insert(root, val);
+    }
+};
