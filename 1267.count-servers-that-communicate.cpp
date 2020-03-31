9886283e07fdd6ebeb465158d54d6f9a6eb694b7
Utsav Krishnan <utsav.krishnan.cse15@iitbhu.ac.in>
2020-03-31 09:14:40 +0530

    update 1267.count-servers-that-communicate.cpp

---
+++ 1267.count-servers-that-communicate.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=1267 lang=cpp
+ *
+ * [1267] Count Servers that Communicate
+ *
+ * https://leetcode.com/problems/count-servers-that-communicate/description/
+ *
+ * algorithms
+ * Medium (58.47%)
+ * Likes:    143
+ * Dislikes: 14
+ * Total Accepted:    10.5K
+ * Total Submissions: 18K
+ * Testcase Example:  '[[1,0],[0,1]]'
+ *
+ * You are given a map of a server center, represented as a m * n integer
+ * matrix grid, where 1 means that on that cell there is a server and 0 means
+ * that it is no server. Two servers are said to communicate if they are on the
+ * same row or on the same column.
+ * 
+ * Return the number of servers that communicate with any other server.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: grid = [[1,0],[0,1]]
+ * Output: 0
+ * Explanation: No servers can communicate with others.
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: grid = [[1,0],[1,1]]
+ * Output: 3
+ * Explanation: All three servers can communicate with at least one other
+ * server.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * 
+ * 
+ * Input: grid = [[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]]
+ * Output: 4
+ * Explanation: The two servers in the first row can communicate with each
+ * other. The two servers in the third column can communicate with each other.
+ * The server at right bottom corner can't communicate with any other
+ * server.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m <= 250
+ * 1 <= n <= 250
+ * grid[i][j] == 0 or 1
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> row(n, 0), col(m, 0);
+
+        for(int i=0; i<n; i++)
+        	for(int j=0; j<m; j++)
+        		row[i] += grid[i][j], col[j] += grid[i][j];
+
+        int cnt = 0;
+        for(int i=0; i<n; i++)
+        	for(int j=0; j<m; j++)
+        		if(grid[i][j] && (row[i]+col[j]-2))
+        			cnt += 1;
+       
+       return cnt;
+    }
+};
+// @lc code=end
